ddacfbb1c9653df02c85f06b7cea7d70c0429f4d
Wallace Rangel <wallacerangelgama7@gmail.com>
2022-10-18 10:54:24 -0300

    ifish-v1.1.0

--- index.php
+++ index.php
@@ -1,16 +1,125 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-BR">
     <head>
         <meta charset="UTF-8">
         <title>IFISH</title>
         <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
     </head>
-    <body>
+    <body class="body">
+        <!--Barra de Navegação -->
+        <header class="header">
+            <img href="index.php" class="ifish-bar" src="img/ifishlogo-red.png" ">
+            <nav class="navbar">
+                <a href="#">home</a>
+                <a href="#">lorem</a>
+                <a href="#">produtos</a>
+                <a href="#">categorias</a>
+                <a href="#">lorem</a>
+                <a href="#">ajuda</a>
+            </nav>
+            <div class="icons">
+                <div class="fas fa-bars" id="menu-btn"></div>
+                <div class="fas fa-search" id="search-btn"></div>
+                <div class="fas fa-shopping-cart" id="cart-btn"></div>
+                <div class="fas fa-user" id="login-btn"></div>
+            </div>
+            <form action="" class="search-form">
+                <input type="search" id="search-box" placeholder="lorem...">
+                <label for="search-box" class="fas fa-search"></label>
+            </form>
+            <div class="shopping-cart">
+                <div class="box">
+                    <i class="fas fa-trash"></i>
+                    <img src="image/cart-img-1.png" alt="">
+                    <div class="content">
+                        <h3>fish</h3>
+                        <span class="preco">R$ 4.99 </span>
+                        <span class="quantidade">qtd : 1</span>
+                    </div>
+                </div>
+                <div class="box">
+                    <i class="fas fa-trash"></i>
+                    <img src="image/cart-img-2.png" alt="">
+                    <div class="content">
+                        <h3>fish</h3>
+                        <span class="preco">R$ 4.99 </span>
+                        <span class="quantidade">qtd : 1</span>
+                    </div>
+                </div>
+                <div class="box">
+                    <i class="fas fa-trash"></i>
+                    <img src="image/cart-img-3.png" alt="">
+                    <div class="content">
+                        <h3>fish</h3>
+                        <span class="preco">R$ 4.99 </span>
+                        <span class="quantidade">qtd : 1</span>
+                    </div>
+                </div>
+                <div class="total"> total : R$ 14.97 </div>
+                <a href="#" class="btn">finalizar</a>
+            </div>
+
+            <form action="" class="login-form">
+                <h3>logue agora</h3>
+                <input type="email" placeholder="your email" class="box">
+                <input type="password" placeholder="your password" class="box">
+                <p>Esqueceu sua senha? <a href="#">Clique Aqui</a></p>
+                <p>Ainda não tem uma conta? <a href="#">crie agora</a></p>
+                <input type="submit" value="logar" class="btn">
+            </form>          
+        </header>       
+        <!--início-->
+        <div class="inicial-main">
+            <!--<div class="icon ">
+                <img class="moon" src="img/moon.png ">
+                <img class="sun" src="img/sun.png ">
+            </div>-->
+            <div class="incial-left">
+                <div class="inicial-text">
+                    <h4 class="text-h4">LoremIpsum</h4>
+                    <h1 class="text-h1">LoremIpsum<br>LoremIpsumm</h1>
+                    <p class="p">LoremIpsumLoremIpsumLoremIpsumLoremIpsum</p>
+                    <a href="#" class="inicial-btn">Lorem Ipsum</a>
+                </div>
+            </div>
+            <div class="inicial-right">    
+                <section class="container">
+                    <div class="slides">
+                        <input type="radio" name="radio" id="radio-1" checked>
+                        <input type="radio" name="radio" id="radio-2">
+                        <input type="radio" name="radio" id="radio-3">
+                        <input type="radio" name="radio" id="radio-4">
+
+                        <div class="imagens one">
+                            <img src="img/slides/1.png" alt="imagens">
+                        </div>
+                        <div class="imagens">
+                            <img src="img/slides/2.png" alt="imagens">
+                        </div>
+                        <div class="imagens">
+                            <img src="img/slides/3.jpg" alt="imagens">
+                        </div>
+                        <div class="imagens">
+                            <img src="img/slides/4.jpg" alt="imagens">
+                        </div>
+                    </div>
+                    <div class="roll-slide">
+                        <label for="radio-1" class="nav"></label>
+                        <label for="radio-2" class="nav"></label>
+                        <label for="radio-3" class="nav"></label>
+                        <label for="radio-4" class="nav"></label>
+                    </div> 
+                </section>   
+            </div>
+        </div>
+        <!--Login-->
         <div class="main-login">
             <div class ="left-img">
-                <img src="img/ifishlogo.png" class="ifish">
+                <img src="img/ifishlogo-red.png" class="ifish">
             </div>
             <form action="conf_login.php" method="POST">
-                <div class ="right-login">  
+                <div class ="right-login">
                     <div class="card-login">
                         <h1>LOGIN</h1>
                         <div class="textfield">
@@ -26,6 +135,7 @@
                     </div>
                 </div>
             </form>
-        </div>    
+        </div>-->
+        <script src="script.js"></script>
     </body>
 </html>
---
+++ script.js
@@ -0,0 +1,106 @@
+//Contador Slide Fotos
+let contador = 1;
+
+setInterval(function() {
+
+    document.getElementById('radio-' + contador).checked = true;
+    contador++;
+
+    if (contador > 4) {
+        contador = 1;
+    }
+
+}, 4000);
+//Darkmode
+/*
+const body = document.querySelector('.body');
+const icon = document.querySelector('.icon');
+
+icon.addEventListener('click', () => {
+    body.classlist.toggle('body-dark');
+})*/
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
