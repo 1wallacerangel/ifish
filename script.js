c08586cd9dc4b57bf51c7ee02c96cff93e465b87
Wallace Rangel <wallacerangelgama7@gmail.com>
2022-10-24 20:23:24 -0300

    ifish-v1.1.7-Login e register working 80%

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ setInterval(function () {
     }
 
 }, 4000);
+
 //Darkmode
 /*
  const body = document.querySelector('.body');
@@ -37,6 +38,7 @@ document.querySelector('#search-btn').onclick = () => {
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
+    logRegister.classList.remove('active');
 }
 
 let shoppingCart = document.querySelector('.shopping-cart');
@@ -46,17 +48,20 @@ document.querySelector('#cart-btn').onclick = () => {
     searchForm.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
+    logRegister.classList.remove('active');
 }
 
 let loginForm = document.querySelector('.login-form');
 let logRegister = document.querySelector('.log-register');
 
 document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active');
+    /*loginForm.classList.toggle('active');*/
     logRegister.classList.toggle('active');
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
     navbar.classList.remove('active');
+    login()
+    
 }
 
 let navbar = document.querySelector('.navbar');
@@ -70,54 +75,14 @@ document.querySelector('#menu-btn').onclick = () => {
 }
 
 window.onscroll = () => {
+
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
+    logRegister.classList.remove('active');
 }
-/*
-var swiper = new Swiper(".product-slider", {
-    loop: true,
-    spaceBetween: 20,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    centeredSlides: true,
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1020: {
-            slidesPerView: 3,
-        },
-    },
-});
 
-var swiper = new Swiper(".review-slider", {
-    loop: true,
-    spaceBetween: 20,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    centeredSlides: true,
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1020: {
-            slidesPerView: 3,
-        },
-    },
-});
-*/
 /*Ocultar e Mostrar Senha*/
 
 const eye = document.getElementById("eye")
@@ -143,7 +108,9 @@ function hidePassword() {
 }
 
 var logi = document.getElementById("login");
+const logtext = document.getElementById("log-p");
 var registe = document.getElementById("register");
+const regtext = document.getElementById("reg-p");
 var btn = document.getElementById("btn");
 
 function register(){
@@ -154,6 +121,7 @@ function register(){
 
 function login(){
     logi.style.left="0px";
-    registe.style.left="-380px";
+    registe.style.left="-385px";
     btn.style.left="20px";
 }
+
