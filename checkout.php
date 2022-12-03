60f2374d0883dd773b76743d613d86bc71aa22e1
Wallace Rangel <wallacerangelgama7@gmail.com>
2022-12-03 02:06:03 -0300

    ifish-v1.4.3-WORKING ON REPONSIVE CSS

--- checkout.php
+++ checkout.php
@@ -65,7 +65,7 @@ if (isset($_POST['order'])) {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>IFISH - Finzalizar</title>
+   <title>IFISH - Finalizar Pedido</title>
    <link rel="shortcut icon" href="img/ifish-icon.png">
    <link rel="stylesheet" href="css/index.css">
    <link rel="stylesheet" href="css/header.css">
