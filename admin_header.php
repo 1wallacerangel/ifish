4e10111b0579d23074c709ae74c3f7563b770b4d
Wallace Rangel <wallacerangelgama7@gmail.com>
2022-12-08 21:57:54 -0300

    ifish-v1.5-6-CSS AND CORRECTIONS ON ADMIN PANEL WORKING

--- admin_header.php
+++ admin_header.php
@@ -1,3 +1,7 @@
+<?php 
+ini_set('display_errors', 0 );
+error_reporting(0);
+?>
 <?php
 
 include 'conexao.php';
@@ -17,14 +21,14 @@ if (isset($_POST['submit_register'])) {
     $select->execute([$email]);
 
     if ($select->rowCount() > 0) {
-        $message_register[] = 'user email already exist!';
+        $message_register[] = 'Email já cadastrado!';
     } else {
         if ($senha != $conf_senha) {
-            $message_register[] = 'confirm password not matched!';
+            $message_register[] = 'As senhas não batem!';
         } else {
             $insert = $conn->prepare("INSERT INTO `usuario`(nome, email, senha) VALUES(?,?,?)");
             $insert->execute([$nome, $email, $senha]);
-            $message_register[] = 'registrado!';
+            $message_register[] = 'Conta criada com sucessor!';
         }
     }
 }
@@ -51,24 +55,21 @@ if (isset($_POST['submit_login'])) {
 
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if($rowCount > 0){
+    if ($rowCount > 0) {
 
-        if($row['user_type'] == 'admin'){
-  
-           $_SESSION['admin_id'] = $row['id'];
-           header('location:admin_index.php');
-  
-        }elseif($row['user_type'] == 'user'){
-  
-           $_SESSION['user_id'] = $row['id'];
-           header('location:index.php');
-  
-        }else{
-           $message[] = 'no user found!';
+        if ($row['user_type'] == 'admin') {
+
+            $_SESSION['admin_id'] = $row['id'];
+            header('location:admin_index.php');
+        } elseif ($row['user_type'] == 'user') {
+
+            $_SESSION['user_id'] = $row['id'];
+            header('location:index.php');
+        } else {
+            $message_login[] = 'Usuário não encontrado!';
         }
-  
-    }else{
-        $message[] = 'incorrect email or password!';
+    } else {
+        $message_login[] = 'Email ou senha incorretos!';
     }
 }
 ?>
@@ -94,7 +95,7 @@ if(isset($_GET['delete'])){
 }
 ?>
 <header class="header">
-    <img href="index.php" class="ifish-bar" src="img/ifishlogo-red.png" ">
+    <img href="index.php" class="ifish-bar-admin" src="img/ifishlogo-admin.png" ">
     <nav class=" navbar">
     <a href="admin_index.php">ínicio</a>
     <a href="admin_produtos.php">produtos</a>
@@ -118,58 +119,58 @@ if(isset($_GET['delete'])){
                 $select_profile = $conn->prepare("SELECT * FROM `usuario` WHERE id = ?");
                 $select_profile->execute([$admin_id]);
                 $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
-                echo '<p class="profile-name">' . $fetch_profile['nome'] . '</p>
-                    <a href="logout.php" class="delete-btn">logout</a>';
+                echo '<p class="profile-nome">' . $fetch_profile['nome'] . '</p>
+                    <a href="admin_atualizar_perfil.php" class="update-btn">Atualizar Perfil</a>
+                    <a href="logout.php" class="logout-btn">sair<i class="fa-solid fa-right-from-bracket"></i></a>';
             } else {
+                echo '
+                <div id="btn"></div>
+                <div class="button-box">
+                    <button type="button" class="toggle-btn" onclick="login()" onclick="login2()">
+                        <p class="log-text" id="log-p">Entrar</p>
+                    </button>
+                    <button type="button" class="toggle-btn" onclick="register()" onclick="register2()">
+                        <p class="reg-text" id="reg-p">Cadastrar</p>
+                    </button>
+                </div>
+                <form action="" id="login" class="login-form" method="POST">';
+
+                if (isset($message_login)) {
+                    foreach ($message_login as $message_login) {
+                        echo '
+                                <div class="mensagem-login">
+                                <span>' . $message_login . '</span>
+                                </div>
+                            ';
+                    }
+                }
+                echo '
+                    <input type="email" name="email" placeholder="Insira seu email" class="box" required>
+                    <input type="password" name="senha" placeholder="Insira sua senha" class="box2" id="senha-id" required>
+                    <div id="eye" class="eye" onclick="eyeClick()"></div>
+                    <p>Esqueceu sua senha? <a href="#">Clique Aqui</a></p>
+                    <input type="submit" name="submit_login" value="Entrar" class="btn">
+                </form>
+                <form action="" method="POST" id="register" class="register-form">';
+
+                if (isset($message_register)) {
+                    foreach ($message_register as $message_register) {
+                        echo '
+                                <div class="mensagem-register">
+                                <span>' . $message_register . '</span>
+                                </div>
+                            ';
+                    }
+                }
+                echo '
+                    <input type="text" name="nome" placeholder="Insira seu nome" class="box-r" required>
+                    <input type="email" name="email" placeholder="Insira seu email" class="box-r" required>
+                    <input type="password" name="senha" placeholder="Insira sua senha" class="box-r" required>
+                    <input type="password" name="conf_senha" placeholder="Confirme sua senha" class="box-r" required>
+                    <input type="submit" name="submit_register" value="Criar Conta" class="btn">
+                    </form>';
             }
             ?>
         </div>
-        <div id="btn"></div>
-        <div class="button-box">
-            <button type="button" class="toggle-btn" onclick="login()" onclick="login2()">
-                <p class="log-text" id="log-p">Logar</p>
-            </button>
-            <button type="button" class="toggle-btn" onclick="register()" onclick="register2()">
-                <p class="reg-text" id="reg-p">Registrar</p>
-            </button>
-        </div>
-        <form action="" id="login" class="login-form" method="POST">
-            <?php
-            if (isset($message_login)) {
-                foreach ($message_login as $message_login) {
-                    echo '
-                        <div class="message">
-                        <span>' . $message_login . '</span>
-                        <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-                        </div>
-                    ';
-                }
-            }
-            ?>
-            <input type="email" name="email" placeholder="Insira seu email" class="box" required>
-            <input type="password" name="senha" placeholder="Insira sua senha" class="box2" id="senha-id" required>
-            <div id="eye" class="eye" onclick="eyeClick()"></div>
-            <p>Esqueceu sua senha? <a href="#">Clique Aqui</a></p>
-            <input type="submit" name="submit_login" value="logar" class="btn">
-        </form>
-        <form action="" method="POST" id="register" class="register-form">
-            <?php
-            if (isset($message_register)) {
-                foreach ($message_register as $message_register) {
-                    echo '
-                        <div class="message">
-                        <span>' . $message_register . '</span>
-                        <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-                        </div>
-                    ';
-                }
-            }
-            ?>
-            <input type="text" name="nome" placeholder="Nome" class="box-r" required>
-            <input type="email" name="email" placeholder="Insira seu email" class="box-r" required>
-            <input type="password" name="senha" placeholder="Insira sua senha" class="box-r" required>
-            <input type="password" name="conf_senha" placeholder="Confirme sua senha" class="box-r" required>
-            <input type="submit" name="submit_register" value="register" class="btn">
-        </form>
     </div>
 </header>
--- admin_index.php
+++ admin_index.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/admin_index.css">
     <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
 </head>
 
